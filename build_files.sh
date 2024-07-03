cc7ce018cac0d24046b76a5366c0ce47bc4d315e
mumiak-mitch <akoth0mitch@gmail.com>
2024-07-03 14:35:18 +0300

    deployment

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pipenv install -r requirements.txt 
-python3.10 manage.py collectstatic
+#!/bin/bash
+
+# Install pipenv if not already installed
+pip install pipenv
+
+# Install dependencies
+pipenv install -r requirements.txt
+
+# Run Django collectstatic
+pipenv run python3.10 manage.py collectstatic --noinput
